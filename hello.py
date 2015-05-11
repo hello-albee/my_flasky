3111b8b5e1d6bf0587ed1ad6d3ac8724d0f733bc
cnnic17173 <cnnic17173@126.com>
2015-05-11 14:46:52 +0800

    第四章，表单

--- hello.py
+++ hello.py
@@ -4,15 +4,26 @@
 # python D:\GitHub\my_flasky\hello.py runserver --host 0.0.0.0
 
 from datetime import datetime
-from flask import Flask,render_template
+from flask import Flask,render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'let me guess'
+
 manager = Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
+
+class NameForm(Form):
+    name = StringField('What\'s your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -22,9 +33,16 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    form=NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+     	   flash('Looks like you have changed your name!')
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',current_time=datetime.utcnow(),form=form,name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
